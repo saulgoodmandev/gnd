c209c61765a5089d6c2c659d58244e31a54b6ab5
acao2002 <xuanan12072002@gmail.com>
2023-01-05 00:30:14 +0700

    finish gmdUSD

--- contracts/gmdUSD.sol
+++ contracts/gmdUSD.sol
@@ -16,6 +16,7 @@ contract gmdUSD is ERC20("gmdUSD", "gmdUSD"), Ownable , ReentrancyGuard{
     address public arbitragor;
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
+    bool public onlyArbitragor = true;
 
     uint256 public vestingPeriod = 365 days;
     IERC20 public gmdUSDC = IERC20(0x3DB4B7DA67dd5aF61Cb9b3C70501B1BdB24b2C22);
@@ -32,6 +33,7 @@ contract gmdUSD is ERC20("gmdUSD", "gmdUSD"), Ownable , ReentrancyGuard{
         require(gDAI.balanceOf(address(this)) <= 50000e18, "max gDai");
         require(_token == gmdUSDC || _token == gDAI, "not pegged token");
         require(_token.balanceOf(msg.sender) >= _amount, "token balance too low");
+        require(_amount <= 5000e18);
         uint256 amountOut = _amount;
         _mint(msg.sender, amountOut);
         _token.safeTransferFrom(msg.sender, address(this), _amount);
@@ -39,7 +41,10 @@ contract gmdUSD is ERC20("gmdUSD", "gmdUSD"), Ownable , ReentrancyGuard{
 
 
     function mint(uint256 _amount, IERC20 _token) external nonReentrant {
-        require(msg.sender == arbitragor, "not arbitragor");
+        if (onlyArbitragor){
+            require(msg.sender == arbitragor, "not arbitragor");
+        }
+        
         require(_token == gmdUSDC || _token == gDAI, "not pegged token");
         require(_token.balanceOf(msg.sender) >= _amount, "token balance too low");
         uint256 amountOut = _amount;
@@ -48,7 +53,9 @@ contract gmdUSD is ERC20("gmdUSD", "gmdUSD"), Ownable , ReentrancyGuard{
     }
 
     function redeem(uint256 _amount, IERC20 _token) external nonReentrant {
-        require(msg.sender == arbitragor, "not arbitragor");
+        if (onlyArbitragor){
+            require(msg.sender == arbitragor, "not arbitragor");
+        }
         require(_token == gmdUSDC || _token == gDAI, "not pegged token");
         require(_token.balanceOf(msg.sender) >= _amount, "token balance too low");
         uint256 amountOut = _amount;
