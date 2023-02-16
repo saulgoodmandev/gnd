8827ea8428d4d84f5c5b3b0c3d50244a35990b72
zombiefinance <zombie.society@proton.me>
2023-02-16 12:42:27 -0800

    wip

--- src/LPToken.sol
+++ src/LPToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
